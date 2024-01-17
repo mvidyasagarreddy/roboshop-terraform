836dbd5d5f4bc06bdf0881d850362682467a4e9b
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-17 07:18:57 +0530

    Roboshop Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "instances" {
-  source = "git::https:github.com/mvidyasagarreddy/tf-module-app.git"
+  source = "git@github.com:mvidyasagarreddy/tf-module-app.git"
   for_each = var.components
   component = each.key
   env = var.env
